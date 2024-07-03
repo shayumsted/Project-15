61172c0e89121721e753f357abe1585e33d24dde
Shamira Umstead <sumstead@usf.edu>
2024-07-03 16:27:06 -0400

    git commit -m "Implement dynamic skills section using props."

--- PersonalInfo.js
+++ PersonalInfo.js
@@ -8,8 +8,8 @@ const PersonalInfo = () => {
     <div className="personal-info">
       <h1> Shamira Umstead </h1>
       <p> I am a Junior majoring in Business Analystics and Information Systems. This class has altered my computer skills, efficiency standards, and Visual Studio Code experience. I also have skills in Microsoft Office, Tableau, and Retail. </p>
-      <p> Email: sumsteadl@usf.edu </p>
-      <p>LinkedIn: <a href="https://linkedin.com/in/shamiraumstead" target="_blank" rel="noopener noreferrer">your-profile</a></p>
+      <p> Email- sumsteadl@usf.edu </p>
+      <p>LinkedIn- <a href="https://linkedin.com/in/shamiraumstead" target="_blank" rel="noopener noreferrer">my-profile</a></p>
     </div>
   );
 };
--- ReactEnvironment.bash
+++ ReactEnvironment.bash
@@ -1,3 +1,4 @@
+// U78784426
 npx create-react-app my-app
 cd my-app
 npm start
